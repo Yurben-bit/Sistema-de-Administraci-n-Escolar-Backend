248cb72ebf323142188f94103a123db7918c7745
Antony-potato <antoniocortazarjimenez@gmail.com>
2026-02-17 17:37:30 -0500

    primer commit del Backend de EduTec

---
+++ src/main/java/com/tecmilenio/edutec/EdutecApplication.java
@@ -0,0 +1,13 @@
+package com.tecmilenio.edutec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdutecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdutecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tecmilenio/edutec/EdutecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecmilenio.edutec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdutecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
